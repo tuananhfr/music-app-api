9cb4cb8e050f789edd66488d72dac74c12615c30
PHAM Tuan Anh <phamtuananh3031998@gmail.com>
2025-03-03 20:14:19 +0100

    feat: init music app for spring boot

---
+++ src/main/java/com/tuananhfr/music/MusicAppApiApplication.java
@@ -0,0 +1,13 @@
+package com.tuananhfr.music;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicAppApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicAppApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tuananhfr/music/MusicAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuananhfr.music;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicAppApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
